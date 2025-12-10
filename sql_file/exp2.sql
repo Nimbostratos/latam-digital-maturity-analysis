98b1098b483e9b95a5abd2add3ee15b87bc2fca4
Enrique Morales Estrada <Quique202@outlook.com>
2025-12-10 17:44:34 -0600

    Add files via upload

---
+++ sql_file/exp2.sql
@@ -0,0 +1,4 @@
+ SELECT DISTINCT t0.indicator_code, t0.indicator_name
+FROM `bigquery-public-data`.`world_bank_wdi`.`indicators_data` AS t0
+WHERE LOWER(t0.indicator_name) LIKE '%internet%'
+LIMIT 20;
